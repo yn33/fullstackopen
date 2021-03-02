172d7d30b3bdf83b28bcf0c27d40dd37b1248e9a
yn33 <69899860+yn33@users.noreply.github.com>
2021-03-02 15:49:32 +0200

    2.04

--- osa2/2.01-2.05/src/index.js
+++ osa2/2.01-2.05/src/index.js
@@ -48,7 +48,7 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
+  const courses = [{
     name: 'Half Stack application development',
     id: 1,
     parts: [
@@ -66,13 +66,38 @@ const App = () => {
       name: 'State of a component',
       exercises: 14,
       id: 3
+    },
+    {
+      name: 'Redux',
+      exercises: 11,
+      id: 4
     }
   ] 
+},
+{
+  name: 'Node.js',
+  id: 2,
+  parts: [
+    {
+      name: 'Routing',
+      exercises: 3,
+      id: 1
+    },
+    {
+      name: 'Middlewares',
+      exercises: 7,
+      id: 2
+    }
+  ]
 }
+]
 
   return (
     <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
       <Course course={course} />
+      )}
     </div>
   )
 }
