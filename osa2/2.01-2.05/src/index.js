cad00bd1a01e52ec56af503d062ffcce0878403c
yn33 <69899860+yn33@users.noreply.github.com>
2021-03-02 15:54:37 +0200

    2.05

--- osa2/2.01-2.05/src/index.js
+++ osa2/2.01-2.05/src/index.js
@@ -1,51 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './Course'
 
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-      {props.parts.map(part => 
-      <Part key={part.id} part={part}/>
-      )}
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-      <b>total of {props.parts.map(part => part.exercises).reduce( (s, p) => s + p)} exercises</b>
-    </>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-  <div>
-    <Header course={course.name}/>
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-  </div>
-  )
-}
 
 const App = () => {
   const courses = [{
