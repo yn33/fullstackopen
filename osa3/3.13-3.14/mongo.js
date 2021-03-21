ac4b95b8b59f2986e65f7eab91b8a908f5f550a6
yn33 <69899860+yn33@users.noreply.github.com>
2021-03-21 18:08:40 +0200

    3.15-3.16

--- osa3/3.13-3.14/mongo.js
+++ osa3/3.13-3.14/mongo.js
@@ -15,5 +15,13 @@ const phoneNumberSchema = new mongoose.Schema({
   number: String
 })
 
+phoneNumberSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('PhoneNumber', phoneNumberSchema)
 
--- osa3/3.15-3.18/index.js
+++ osa3/3.15-3.18/index.js
@@ -13,19 +13,25 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 const PhoneNumber = require('./mongo')
 const PORT = process.env.PORT
   
-  app.get('/api/persons', (req, res) => {
+  app.get('/api/persons', (req, res, next) => {
     PhoneNumber.find({}).then(persons => {
       res.json(persons)
     })
+    .catch(error => next(error))
   })
 
-  app.get('/api/persons/:id', (req, response) => {
+  app.get('/api/persons/:id', (req, res, next) => {
     PhoneNumber.findById(req.params.id).then(person => {
-      res.json(person)
+      if(person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
   
-  app.post('/api/persons', (req, res) => {
+  app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if (!body.name) {
@@ -50,16 +56,18 @@ const PORT = process.env.PORT
     person.save().then(savedPerson => {
       res.json(savedPerson)
     })
+    .catch(error => next(error))
 
   })
 
-  app.delete('/api/persons/:id', (req, res) => {
-    Note.findByIdAndRemove(req.params.id)
+  app.delete('/api/persons/:id', (req, res, next) => {
+    PhoneNumber.findByIdAndRemove(req.params.id)
     .then(result => {
       res.status(204).end()
     })
+    .catch(error => next(error))
   })
-  
+
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
